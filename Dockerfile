b8d53fd8d59d524d26ebeb6b9ac4c824ad7686fb
Jacob <infojacob2020@gmail.com>
2024-12-10 18:28:19 +1300

    With Docker (own network)

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:18-alpine
+
+WORKDIR /usr/src/app
+COPY ./node/package*.json ./
+RUN npm install
+COPY ./node ./
+
+EXPOSE 3000
+CMD ["node", "server.js"]
--- node/src/controllers/indexController.js
+++ node/src/controllers/indexController.js
@@ -4,11 +4,19 @@ const session = require('express-session');
 exports.indexGet = (req, res) => {
     const dateTime = new Date();
     let welcomeMSG;
+	let clientIp = req.ip;
+	if (clientIp.startsWith('::ffff:')) {
+		clientIp = clientIp.split(':').pop(); // Extract the IPv4 part
+	}
     if (req.session.active) {
         welcomeMSG = "Welcome back!";
     } else {
         welcomeMSG = "Welcome!";
         req.session.active = true;
     }
-    return res.render('index', {welcomeMSG: welcomeMSG, dateTime: dateTime.toString()});
+    return res.render('index', {
+		clientIp: clientIp, 
+		welcomeMSG: welcomeMSG, 
+		dateTime: dateTime.toString()
+	});
 }
