edd3ffafbf27efc989ae02c3fa7b366a13810a23
Jacob <infojacob2020@gmail.com>
2025-08-29 22:40:16 +1200

    Simplified index controller code, added a favicon with cache busting, changed index CSS to be more use case friendly.

--- node/src/controllers/indexController.js
+++ node/src/controllers/indexController.js
@@ -1,22 +1,12 @@
 import 'dotenv/config';
-import session from 'express-session';
 
 export function indexGet(req, res) {
-    const dateTime = new Date();
     let welcomeMSG;
-    let clientIp = req.ip;
-    if (clientIp.startsWith('::ffff:')) {
-        clientIp = clientIp.split(':').pop(); // Extract the IPv4 part
-    }
     if (req.session.active) {
-        welcomeMSG = "Welcome back!";
+        welcomeMSG = "Welcome back";
     } else {
-        welcomeMSG = "Welcome!";
+        welcomeMSG = "Welcome";
         req.session.active = true;
     }
-    return res.render('index', {
-        clientIp: clientIp,
-        welcomeMSG: welcomeMSG,
-        dateTime: dateTime.toString()
-    });
+    return res.render('index', { welcomeMSG: welcomeMSG });
 }
